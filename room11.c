cd7a92a5e38e38982c1360d4cf00f76825f31fe9
Maik De Leon Lopez <mdeleonlopez1@nsmb208-xx.csudh.edu>
2018-03-19 16:34:08 -0700

    version 1

---
+++ room11.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void main(void)
+{
+	int numChoice;
+	puts("a damp room, mostly empty, three boxes lay on the floor");
+	puts("you feel the urge to look through them");
+	puts("pick one of the boxes");
+	scanf("%d",numChoice);
+	if(numChoice==1)
+	{
+		puts("an empty box");
+	}else if(numChoice==2)
+	{
+		
+	}else
+	{
+		
+	}
+
+	
+}
